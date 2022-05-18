25f059c1d4f3804723011c3bb3bb1bc737c25c7e
tuvaw <tuvawieder@hotmail.com>
2022-05-18 12:08:43 +0200

    Added react-bootstrap

--- synonyms-app/src/App.js
+++ synonyms-app/src/App.js
@@ -1,8 +1,20 @@
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
+
+    <Container>
       <h1>Get synonyms</h1>
+      <Row>
+        <h2>hej</h2>
+      </Row>
+      <Row>
+        <Col sm>sm=true</Col>
+        <Col sm>sm=true</Col>
+        <Col sm>sm=true</Col>
+      </Row>
+    </Container>
     </div>
   );
 }
