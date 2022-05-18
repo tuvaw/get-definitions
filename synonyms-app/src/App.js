dd25c8ada93d8a34376ab9f98ac9e340a70d97ca
tuvaw <tuvawieder@hotmail.com>
2022-05-18 12:31:13 +0200

    Some changes

--- synonyms-app/src/App.js
+++ synonyms-app/src/App.js
@@ -1,18 +1,36 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import SearchList from './searchList'
+import { Container, Row, Col, Form } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
 
     <Container>
-      <h1>Get synonyms</h1>
+      <h1>Hitta synonymer</h1>
       <Row>
-        <h2>hej</h2>
+        <>
+          <Form.Label htmlFor="inputSearch">Vilket ord vill du söka på?</Form.Label>
+          <Form.Control
+            type="input"
+            id="inputSearch"
+            aria-describedby="searchHelpBlock"
+          />
+          <Form.Text id="searchHelpBlock" muted>
+            Ange vilket ord du vill hitta synonymer på så bjuder vi på en gif!
+          </Form.Text>
+        </>
       </Row>
       <Row>
-        <Col sm>sm=true</Col>
-        <Col sm>sm=true</Col>
-        <Col sm>sm=true</Col>
+        <h3>Visa sökningen</h3>
+        <Col sm>Ord</Col>
+        <Col sm>Gif</Col>
+      </Row>
+      <Row>
+        <h3>Senaste sökningar</h3>
+        <SearchList />
+          <Col sm>sm=true</Col>
+          <Col sm>sm=true</Col>
+          <Col sm>sm=true</Col>
       </Row>
     </Container>
     </div>
