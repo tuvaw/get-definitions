90d09c31e0ff55cdc6e068004c26fc0036aed293
tuvaw <tuvawieder@hotmail.com>
2022-05-23 10:41:08 +0200

    Påbörjat api till synonymer

--- synonyms-app/src/synonyms.js
+++ synonyms-app/src/synonyms.js
@@ -1,3 +1,21 @@
-export default function Synonyms() {
-    console.log("Hej")
-}
+import $ from "jquery"
+import { useRef } from 'react';
+
+export default function Synonyms(event) {
+    const inputRef = useRef();
+    inputRef.on("keyup", function() {
+
+        event.preventDefault();
+        if (event.keyCode === 13){ 
+            $.ajax({
+                url: "synonymord.se/api/?q=" + inputRef,
+                method: 'GET',
+                dataType: "JSON"           
+            }).done(function(data) {
+                for (let i = 0; i < data.Search.length; i++) {
+                    // Lägg till i sökresultat
+                }
+            });
+        }
+    }) 
+};
